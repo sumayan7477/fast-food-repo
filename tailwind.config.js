557c7956d8dee3211eced9f343aee8b92173c016
sumayan7477 <sumayan7477@gmail.com>
2024-03-02 21:28:06 +0600

    first commit

---
+++ tailwind.config.js
@@ -0,0 +1,17 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+    content: ["./src/**/*.{html,js}"],
+    theme: {
+      extend: {
+        colors:{
+
+          first:{
+            100:'#FAAB78'
+          }
+        
+        }
+      },
+      
+    },
+    plugins: [],
+  }
